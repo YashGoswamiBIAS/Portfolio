15896c7ae92a5ca23a6c67e6643b97486add8fb1
YashGoswamiBIAS <100066481+YashGoswamiBIAS@users.noreply.github.com>
2022-04-13 12:56:06 +0530

    Add files via upload

--- loader.js
+++ loader.js
@@ -2,8 +2,54 @@ var welcome = document.querySelector(".welcome") ;
 var loader = document.querySelector(".loader") ;
 var circle = document.querySelector(".circle") ;
 var cut = document.querySelector(".cut") ;
-
+var title = document.querySelector(".title") ;
+var subtitle = document.querySelector(".subtitle") ;
 var body = document.querySelector("body") ;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+var welcometexts = async () => {
+    subtitle.innerHTML = "" ;
+    text = "I am Yash goswami" ;
+    for (let i=0; i< text.length; i++)
+    {
+        subtitle.innerHTML = subtitle.innerHTML + text[i] ;
+        await sleep(100);
+    }
+    await  sleep(1000);
+    subtitle.innerHTML = "" ;
+    text = "I am a Student" ;
+    for (let i=0; i< text.length; i++)
+    {
+        subtitle.innerHTML = subtitle.innerHTML + text[i] ;
+        await sleep(100);
+    }
+    await  sleep(1000);
+    subtitle.innerHTML = "" ;
+    text = "Situated in Hisar, Haryana" ;
+    for (let i=0; i< text.length; i++)
+    {
+        subtitle.innerHTML = subtitle.innerHTML + text[i] ;
+        await sleep(100);
+    }
+    await  sleep(1000);
+    subtitle.innerHTML = "" ;
+    text = "This is my portfolio" ;
+    for (let i=0; i< text.length; i++)
+    {
+        subtitle.innerHTML = subtitle.innerHTML + text[i] ;
+        await sleep(100);
+    }
+    await  sleep(1000);
+    subtitle.innerHTML = "" ;
+    text = "Explore..." ;
+    for (let i=0; i< text.length; i++)
+    {
+        subtitle.innerHTML = subtitle.innerHTML + text[i] ;
+        await sleep(100);
+    }
+} ;
 
 body.style.overflow = "hidden" ;
 document.addEventListener('DOMContentLoaded', function() {
@@ -21,6 +67,13 @@ document.addEventListener('DOMContentLoaded', function() {
             {
                 cut.style.animation = "opening 1s";
             }
+            setTimeout(()=>{
+                title.style.opacity = 1 ;
+                setTimeout(()=>{
+                    subtitle.style.opacity = 1 ;
+                    welcometexts() ;
+                    },500);
+            },1000) ;
         },1000);
     },500)
 }, false);
